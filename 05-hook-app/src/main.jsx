77e061a278e70a5cc080cdfacb42f1f2d62cde2c
gastonfuentes <gastonnicolasfuentes@gmail.com>
2024-06-20 18:11:04 -0300

    hook useState

---
+++ 05-hook-app/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { HooksApp } from './HooksApp.jsx'
+import { CounterApp } from './01-useState/CounterApp.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HooksApp />
+    <CounterApp />
+  </React.StrictMode>,
+)
