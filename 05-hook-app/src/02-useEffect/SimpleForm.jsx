6169dddc4b0a426aa05b274e41f0bdfc447dd195
gastonfuentes <gastonnicolasfuentes@gmail.com>
2024-06-21 19:27:01 -0300

    useEffect

---
+++ 05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        username: 'gasti',
+        email: 'gasti@gasti.com'
+    })
+
+    const { username, email } = formState
+
+    const onInputChange = (event) => {
+
+        const { name, value } = event.target
+
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+
+
+    useEffect(() => {
+        console.log('useeffect se llamo!');
+    }, [])
+
+
+    useEffect(() => {
+        console.log('form changed!');
+    }, [formState])
+
+
+    useEffect(() => {
+        console.log('email changed!');
+    }, [email])
+
+
+    return (
+        <>
+            <h1>Form simple</h1>
+            <hr />
+
+            <input type="text" className="form-control" placeholder="Username" name="username" value={username} onChange={onInputChange} />
+            <input type="email" className="form-control mt-2" placeholder="gas@gas.com" name="email" value={email} onChange={onInputChange} />
+
+        </>
+    )
+}
--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+/* import React from 'react' */
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './HooksApp.jsx'
+/* import { HooksApp } from './HooksApp.jsx' */
 /* import { CounterApp } from './01-useState/CounterApp.jsx' */
-import { CounterWhithCustomHook } from './01-useState/CounterWhithCustomHook.jsx'
+/* import { CounterWhithCustomHook } from './01-useState/CounterWhithCustomHook.jsx' */
+import { SimpleForm } from './02-useEffect/SimpleForm.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HooksApp />
-    <CounterWhithCustomHook />
-  </React.StrictMode>,
+  //<React.StrictMode>   
+  <SimpleForm />
+  //</React.StrictMode>,
 )
